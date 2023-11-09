58e8dc39e2ceff5645354b150b5c4f5328aac4e0
leonardo <leoleitao1227@gmail.com>
2023-11-09 14:45:52 -0300

    add folder

---
+++ Kotlin controles de fluxo/conceitosIniciais/src/main/kotlin/tiposDeVariaveis.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var x: Int // mutavel
+    val y: Int // imutavel
+    var a: Float
+    var b: Double
+    var c: Long
+    var d: Char
+    var e: String
+    var f: Short
+    var g: Byte
+    var h: Byte
+    var i: UInt
+    
+
+}
