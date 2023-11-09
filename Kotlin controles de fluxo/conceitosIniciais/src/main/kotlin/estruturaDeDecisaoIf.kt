964a572870a8345f91313f3321330a51a7d1c385
leonardo <leoleitao1227@gmail.com>
2023-11-09 15:24:50 -0300

    add folder

---
+++ Kotlin controles de fluxo/conceitosIniciais/src/main/kotlin/estruturaDeDecisaoIf.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var idade:Int = 16
+    if(idade>=18){
+        println("Maior de idade")
+    }else if(idade==17){
+        println("Quase maior de idade")
+    }else{
+        println("Menor de idade")
+    }
+}
