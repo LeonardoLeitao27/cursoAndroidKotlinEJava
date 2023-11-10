dac2872356303987938a65c714c52907477f4102
leonardo <leoleitao1227@gmail.com>
2023-11-10 15:11:09 -0300

    add new class

---
+++ Kotlin controles de fluxo/conceitosIniciais/src/main/kotlin/Casa.kt
@@ -0,0 +1,23 @@
+fun casa(){
+    print("Entrando na casa...\n")
+    quarto()
+}
+fun quarto(){
+    print("Passando pelo quarto...\n")
+    guardaRoupa()
+}
+
+fun guardaRoupa(){
+    print("Passando pelo guarda roupa\n")
+    sapato()
+}
+fun sapato(){
+    print("Escolhendo o sapoto\n")
+    cadarco()
+}
+fun cadarco(){
+    print("Escolhendo a cor do cardaco")
+}
+fun main() {
+    casa()
+}
