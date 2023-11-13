e46b72cccaa3e3a9f9532d69cb03960fb9fb309f
leonardo <leoleitao1227@gmail.com>
2023-11-13 11:06:29 -0300

    add new class

---
+++ Kotlin controles de fluxo/conceitosIniciais/src/main/kotlin/matrizChar.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val linhas = 3
+    val colunas = 3
+    val matriz:  Array<Array<Char>> = Array(linhas){Array(colunas) {' '} }
+    var c:Char =  'a'
+
+
+        for (i in 0..linhas-1){
+            for (j in  0..colunas-1){
+                matriz[i][j] = c
+                c++
+            }
+        }
+    println()
+    for (i in 0..linhas-1){
+        for (j in  0..colunas-1){
+            print("${matriz[i][j]} ")
+        }
+        println()
+    }
+
+    println()
+    
+    for(linhas in matriz){
+        for (valor in linhas){
+            print("${valor} ")
+        }
+        println()
+    }
+
+}
