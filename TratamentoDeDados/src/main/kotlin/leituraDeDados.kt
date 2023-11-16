5208777b6ab5a463918c05c3354f3b97940ca05d
leonardo <leoleitao1227@gmail.com>
2023-11-16 15:23:29 -0300

    add new class

---
+++ TratamentoDeDados/src/main/kotlin/leituraDeDados.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val n = readLine()
+    var num = readLine()?.toInt() ?: 0
+    var num2 = readlnOrNull()?.toInt() ?: 0
+    var num3 = readlnOrNull()?.toDouble() ?: 0.0
+    var num4 = readlnOrNull()?.toFloat() ?: 0.0f
+    var num5 = readlnOrNull()?.toBoolean() ?: false
+
+    println("Tipo String $n")
+    println("Tipo int $num")
+    println("Tipo int $num2")
+    println("Tipo double $num3")
+    println("Tipo float $num4")
+    println("Tipo boolean $num5")
+
+}
