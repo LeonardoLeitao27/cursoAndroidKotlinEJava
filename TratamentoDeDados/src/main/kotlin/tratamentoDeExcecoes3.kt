16582f07426cade0c51c3d1331813d60155bab1f
Leonardo <leoleitao1227@gmail.com>
2023-11-21 12:53:30 -0300

    add new class

--- TratamentoDeDados/src/main/kotlin/tratamentoDeExcecoes3.kt
+++ TratamentoDeDados/src/main/kotlin/tratamentoDeExcecoes3.kt
@@ -1,3 +1,24 @@
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.Scanner
+
+fun lerArquivo(){
+    var arquivo = File("C:\\Users\\leoce\\OneDrive\\Área de Trabalho\\codes\\cursoAndroidKotlinEJava\\TratamentoDeDados\\src\\main\\kotlin\\passoAPasso.txt")
+
+    try {
+        Scanner(arquivo).use { l ->
+            while (l.hasNext()){
+                println(l.next())
+            }
+            l.close()
+        }
+
+    }catch (e: FileNotFoundException){
+        println("Falha ao ler o arquivo")
+        //mostra o qual erro ocorreu
+        e.printStackTrace()
+    }
+}
 fun main() {
-    
+    lerArquivo()
 }
