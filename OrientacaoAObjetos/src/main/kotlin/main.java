857425b0f00e76d3cb5aed449cca43c7524ffa50
Leonardo <leoleitao1227@gmail.com>
2023-12-01 15:50:48 -0300

    add new class

---
+++ OrientacaoAObjetos/src/main/kotlin/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+    planeta p = new planeta();
+    System.out.println(p.getNome());
+    p.setNome_("Planeta terra");
+    System.out.println(p.getNome());
+
+    }
+}
---
+++ OrientacaoAObjetos/src/main/kotlin/planeta.java
@@ -0,0 +1,11 @@
+public class planeta {
+    String nome = "Teste";
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome_(String nome){
+        this.nome = nome;
+
+    }
+}
