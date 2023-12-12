c6e6374b0ceac91539f3640c1b911f3baa71d5a2
Leonardo <leoleitao1227@gmail.com>
2023-12-12 14:18:52 -0300

    add new class

--- OrientacaoAObjetos/src/main/kotlin/inicioTardio.kt
+++ OrientacaoAObjetos/src/main/kotlin/inicioTardio.kt
@@ -1,3 +1,20 @@
+class robo{
+    lateinit var nome:String
+    fun ligar(){
+        nome = "Iniciando o robo"
+    }
+
+    fun inicioTardio(){
+        if(!this::nome.isInitialized){
+            nome = "Inicializando tardiamente..."
+            println(nome)
+        }
+    }
+}
 fun main() {
-    
+    var bot:robo = robo()
+
+    //println(bot.ligar())
+    bot.inicioTardio()
+    println(bot.nome)
 }
