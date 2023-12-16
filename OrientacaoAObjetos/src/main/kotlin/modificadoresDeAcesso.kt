a5ecdd1bef34080f1aafe9b495f65f21b3765500
leonardo <leoleitao1227@gmail.com>
2023-12-16 13:27:41 -0300

    add new class

--- OrientacaoAObjetos/src/main/kotlin/modificadoresDeAcesso.kt
+++ OrientacaoAObjetos/src/main/kotlin/modificadoresDeAcesso.kt
@@ -1,4 +1,16 @@
+//private, public e protected
+private class F(){
+    //estará disponivel somente no arquivo()
+    var teste = "classe F"
+}
 open class Eletronico2(var marca:String){
+    private fun ativarCorrente(){
+
+    }
+    protected fun relogio(){
+        //está disponivel para a classe que recebeu a herança
+        println("Função relogio")
+    }
     fun ligar(){
         println("Está ligado")
     }
@@ -6,10 +18,12 @@ open class Eletronico2(var marca:String){
         println("Estpa desligado")
     }
     var modelo:String = "Teste de gerança"
+    private var F:String = "Teste"
 }
-class Computador2(marca:String):Eletronico(marca){
+class Computador2(marca:String):Eletronico2(marca){
     fun instalarSoftware(){
         println("Instalando softwares no computador")
+        relogio()
     }
     fun Varredura(){
         println("Realizando varrefudura")
@@ -22,4 +36,10 @@ fun main() {
     c.Varredura()
     c.instalarSoftware()
     println(c.modelo)
+    //c.f()
+    //c.ativarCorrente()
+    var t:F = F()
+    println(t.teste)
+
+
 }
---
+++ OrientacaoAObjetos/src/main/kotlin/testeDeVisibilidade.kt
@@ -0,0 +1,5 @@
+fun main() {
+    //USADO NA AULA DE MODIFICADORES DE ACESSO
+    /* var t:F = F()
+    println(t.teste) */
+}
