1aea40777919cdbebc72c2da93ab436555d154af
leonardo <leoleitao1227@gmail.com>
2023-12-31 13:46:22 -0300

    add nes class

---
+++ JavaIntro/src/comentario.java
@@ -0,0 +1,14 @@
+public class comentario {
+    public static void main(String[] args) {
+        //comentario em linha
+        System.out.println();
+    }
+}
+
+/*
+comenterio em bloco
+ */
+
+/**
+ * JAVADOC - Comentario voltado a documentação do arquivo
+ */
