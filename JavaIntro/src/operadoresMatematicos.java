3a82dea7ed859125300ff1bd4c40a54dbac5a982
leonardo <leoleitao1227@gmail.com>
2024-01-03 19:57:24 -0300

    add new class

---
+++ JavaIntro/src/operadoresMatematicos.java
@@ -0,0 +1,13 @@
+public class operadoresMatematicos {
+    public static void main(String[] args) {
+
+        System.out.println("5 + 2 = " + (5+2));
+        System.out.println("5 - 2 = " + (5-2));
+        System.out.println("5 * 2 = " + (5*2));
+        System.out.println("5 / 2 = " + (5/2));
+        System.out.println("5 % 2 = " + (5%2));
+
+
+
+    }
+}
