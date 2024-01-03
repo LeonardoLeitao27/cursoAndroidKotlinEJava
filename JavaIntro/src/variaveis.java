f58f4cbddcf384dba2835bf0e3e9610e3b27c925
leonardo <leoleitao1227@gmail.com>
2024-01-03 19:39:09 -0300

    add new class

---
+++ JavaIntro/src/variaveis.java
@@ -0,0 +1,24 @@
+public class variaveis {
+    public static void main(String[] args) {
+        String nome = "Leonardo";
+
+    }
+}
+
+
+/**
+ * TIPOS DE DADOS
+ * 
+ * Byte
+ * Double
+ * Float
+ *
+ * Long
+ * Integer
+ * Short
+ * Byte
+ *
+ * Boolean
+ * String
+ * Char
+ */
