0b95fa47df84d991257ef9cea491b62c00134ec6
Leonardo <leoleitao1227@gmail.com>
2024-01-08 14:22:10 -0300

    add new class

---
+++ JavaIntro/src/estruturaDoWhile.java
@@ -0,0 +1,9 @@
+public class estruturaDoWhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do{
+            System.out.println(a+ " ");
+            a++;
+        }while (a!=10);
+    }
+}
