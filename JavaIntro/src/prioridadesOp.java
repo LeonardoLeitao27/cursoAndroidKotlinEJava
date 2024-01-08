b1cec9bb6c98fd7ea003be92d819ea232bf4fc96
Leonardo <leoleitao1227@gmail.com>
2024-01-08 10:56:00 -0300

    add new class

---
+++ JavaIntro/src/prioridadesOp.java
@@ -0,0 +1,8 @@
+public class prioridadesOp {
+    public static void main(String[] args) {
+        // precedencias de sinais
+        // divisão > multiplicação > resto da divisão > adição ou subtração
+        int x = 10/2*3+4*3; //
+        System.out.println(x);
+    }
+}
---
+++ heranca/src/main/kotlin/escopo.java
@@ -0,0 +1,8 @@
+public class escopo {
+    static int valor=0;
+    int value=0;
+    public escopo(){
+        valor++;
+        value++;
+    }
+}
