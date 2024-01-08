e909386531c207def30b50d2f7ac9c5096c7af3a
Leonardo <leoleitao1227@gmail.com>
2024-01-08 11:51:16 -0300

    add new class

---
+++ JavaIntro/src/funcoesString.java
@@ -0,0 +1,7 @@
+public class funcoesString {
+    public static void main(String[] args) {
+        String str = "Curso de android";
+        System.out.println("Tamanho da String: "+str.length());
+        System.out.println(str.charAt(2));
+    }
+}
