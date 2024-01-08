415f0e26f3d068891397f0cca689b010ad9e0b74
Leonardo <leoleitao1227@gmail.com>
2024-01-08 14:35:35 -0300

    add new class

---
+++ JavaIntro/src/enhacedFor.java
@@ -0,0 +1,8 @@
+public class enhacedFor {
+    public static void main(String[] args) {
+        int n[] = {1,2,3,4,5,6,7,8,9};
+        for (int v:n){
+            System.out.print(v+ " ");
+        }
+    }
+}
