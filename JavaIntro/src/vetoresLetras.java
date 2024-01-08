c32e3dbb2eab603ccada72ece53c330759cbbb73
Leonardo <leoleitao1227@gmail.com>
2024-01-08 14:48:19 -0300

    add new class

---
+++ JavaIntro/src/vetoresLetras.java
@@ -0,0 +1,9 @@
+public class vetoresLetras {
+    public static void main(String[] args) {
+        char letras[] = {'a','b','c'};
+
+        for (int i = 0;i<letras.length;i++){
+            System.out.println(letras[i]);
+        }
+    }
+}
