14037e38e5edd275cb487c62b13578640808c07c
Leonardo <leoleitao1227@gmail.com>
2024-01-08 11:00:30 -0300

    add new class

---
+++ JavaIntro/src/formatacaoString.java
@@ -0,0 +1,12 @@
+public class formatacaoString {
+    public static void main(String[] args) {
+        String a = "Olá, ";
+        String b = "Felipe";
+        System.out.println(a+b);
+
+        //o java ajuda a entender o condeudo da string concatenando ela por partes
+        String palavraGrande = "daksndkasndnajsdnajsdnajsndjansdjn" +
+                "ajsdnjasndjnasdnajsdnajsndjansdjnajsdnjansdjansjdnasjdnajnsdjansj" +
+                "dnajsndjasndjansjdnajsdnajnsdjansdjansdnasndjansjdnajsndjandjasndjansdjad"
+    }
+}
