2a7274cbe18c4d4fd80831ff957d08f85ac598ab
Leonardo <leoleitao1227@gmail.com>
2024-01-08 14:44:48 -0300

    add new class

---
+++ JavaIntro/src/VetoresNumeros.java
@@ -0,0 +1,14 @@
+public class VetoresNumeros {
+    public static void main(String[] args) {
+        //int x=50,z=40,a1=100,t=5;
+        int[] v = new int[4];
+        v[0] = 50;
+        v[1] = 150;
+        v[2] = 250;
+        v[3] = 350;
+
+        for (int i=0;i<v.length;i++){
+            System.out.println(v[i]);
+        }
+    }
+}
