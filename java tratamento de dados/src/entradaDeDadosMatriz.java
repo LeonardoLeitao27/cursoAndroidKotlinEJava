18025ce2d8663532f0a1434210826ac3968f6331
Leonardo <leoleitao1227@gmail.com>
2024-01-12 10:16:44 -0300

    add new class

--- java tratamento de dados/src/entradaDeDadosMatriz.java
+++ java tratamento de dados/src/entradaDeDadosMatriz.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class entradaDeDadosMatriz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tamanho;
+        System.out.println("Digite o tamanho da matriz");
+        tamanho = sc.nextInt();
+
+        int [][] matriz = new int[tamanho][tamanho];
+
+        for (int i = 0;i<tamanho;i++){
+            for(int j = 0;j<tamanho;j++){
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0;i<tamanho;i++){
+            for(int j = 0;j<tamanho;j++){
+                System.out.print(matriz[i][j]+ " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
 }
