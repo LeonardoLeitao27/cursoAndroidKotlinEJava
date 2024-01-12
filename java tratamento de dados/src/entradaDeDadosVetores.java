a6253dbb590aae2eac85edb7d1454d844e87d7b1
Leonardo <leoleitao1227@gmail.com>
2024-01-12 10:12:21 -0300

    add new class

---
+++ java tratamento de dados/src/entradaDeDadosVetores.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class entradaDeDadosVetores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tamanho;
+        System.out.println("Digite o tamanho do vetor");
+        tamanho = sc.nextInt();
+
+        int [] vetor = new int[tamanho];
+
+        for (int i = 0;i<tamanho;i++){
+            vetor[i] = sc.nextInt();
+        }
+        
+        for (int i = 0;i<tamanho;i++){
+            System.out.println(vetor[i]);
+        }
+
+    }
+}
