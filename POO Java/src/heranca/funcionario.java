43bdf5d22a988cdcaf3fad4ef6bc91e5c225d6cd
Leonardo <leoleitao1227@gmail.com>
2024-01-15 15:09:42 -0300

    add new class

--- POO Java/src/heranca/funcionario.java
+++ POO Java/src/heranca/funcionario.java
@@ -1,6 +1,9 @@
 package heranca;
 
 public class funcionario {
+     //usando o final na classe ela não consegue mais fazer a herança
+     //usando o final em uma variavel ela se torna imutavel
+     //usando o final em metodos eles não podem ser sobreescritos com o override
      public String nome;
      protected int idade;
      protected float salario;
