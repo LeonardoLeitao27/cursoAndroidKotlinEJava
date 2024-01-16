e637eac6bb4071c285173b95d96790e972447d9d
Leonardo <leoleitao1227@gmail.com>
2024-01-16 13:52:19 -0300

    add app android

--- meu_app/app/src/main/java/com/leoleitao/meu_app/MainActivity.kt
+++ meu_app/app/src/main/java/com/leoleitao/meu_app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.leoleitao.meu_app
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
