f019faf5865ac74bbf8aa6e2602b15353807fdac
Leonardo <leoleitao1227@gmail.com>
2024-01-24 16:42:31 -0300

    app travel update layout

--- travel_Consuption/app/src/main/java/com/leoleitao/travelconsuption/MainActivity.kt
+++ travel_Consuption/app/src/main/java/com/leoleitao/travelconsuption/MainActivity.kt
@@ -2,10 +2,19 @@ package com.leoleitao.travelconsuption
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.leoleitao.travelconsuption.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding.editDitancia
+
+    }
+
+    fun calculate(){
+        binding.editAutonomia
     }
 }
