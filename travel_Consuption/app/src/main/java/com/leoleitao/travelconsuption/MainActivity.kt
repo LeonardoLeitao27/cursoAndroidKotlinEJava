75678de59ee0b70b1df2f04f935b31d946151bc2
leonardo <leoleitao1227@gmail.com>
2024-01-24 18:37:51 -0300

    color

--- travel_Consuption/app/src/main/java/com/leoleitao/travelconsuption/MainActivity.kt
+++ travel_Consuption/app/src/main/java/com/leoleitao/travelconsuption/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.leoleitao.travelconsuption
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.leoleitao.travelconsuption.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnCalcular.setOnClickListener(this)
+
+    }
+    override fun onClick(view: View) {
+        if(view.id ==R.id.btnCalcular){
+            calculate()
+        }
     }
 
-    fun calculate(){
-        binding.editAutonomia
+    private fun calculate(){
+        Toast.makeText(this, "Você realizou um click", Toast.LENGTH_LONG).show()
     }
+
+
 }
